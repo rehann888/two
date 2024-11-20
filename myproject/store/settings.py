5b051e7c50182109edb71dbbca90c238b49f6288
Reyhan <133482597+rehann888@users.noreply.github.com>
2024-11-20 22:37:37 +0700

    Update settings.py

--- myproject/store/settings.py
+++ myproject/store/settings.py
@@ -130,12 +130,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Pastikan port dan database Redis benar
+        'LOCATION': 'redis://127.0.0.1:6379/1', 
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
 }
 
-# Tambahkan timeout default (5 menit)
 CACHE_TTL = 300
